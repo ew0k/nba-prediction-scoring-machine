e36d11948c796d0186cb4e11a56877cc15f69000
root <root@localhost.localdomain>
2019-10-03 11:47:07 -0400

    Get NBA standings and parse working

---
+++ get-nba-standings.py
@@ -0,0 +1,34 @@
+import json
+import os
+
+sportradar_key = os.environ.get('SPORTRADAR_KEY')
+os.system("curl -X GET \"https://api.sportradar.us/nba/trial/v7/en/seasons/2018/REG/standings.json?api_key={" + sportradar_key + "}\" > nba-standings.json")
+
+standings_file = open("nba-standings.json", "r")
+
+json_string = json.load(standings_file)
+
+west_conference_standings = {}
+east_conference_standings = {}
+
+for conference in json_string["conferences"]:
+    for division in conference["divisions"]:
+        for team in division["teams"]:
+            team_name = team["name"]
+            team_conference_standing = team["calc_rank"]["conf_rank"]
+            if conference["name"].lower() == "western conference": 
+                west_conference_standings[team_conference_standing] = team_name
+            if conference["name"].lower() == "eastern conference":
+                east_conference_standings[team_conference_standing] = team_name
+
+print("West Standings")
+for standing in range(1, 15+1):
+    team_name = west_conference_standings[standing]
+    print(team_name + ": " + str(standing))
+
+print()
+print("East Standings")
+for standing in range(1, 15+1):
+    team_name = east_conference_standings[standing]
+    print(team_name + ": " + str(standing))
+
