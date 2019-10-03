8ce27d66a14f3f0c15094adfd067f60de5a0f266
root <root@localhost.localdomain>
2019-10-03 16:16:00 -0400

    Update code to grab data with python

--- get-nba-standings.py
+++ get-nba-standings.py
@@ -1,35 +1,44 @@
 import json
 import os
+import http.client
 
-standings_file_name = "nba-standings.json"
-sportradar_key = os.environ.get('SPORTRADAR_KEY')
-os.system("curl -X GET \"https://api.sportradar.us/nba/trial/v7/en/seasons/2018/REG/standings.json?api_key={" + sportradar_key + "}\" > " + standings_file_name)
+def get_standings():
+    conn = http.client.HTTPSConnection("api.sportradar.us")
 
-standings_file = open(standings_file_name, "r")
+    conn.request("GET", "/nba/trial/v7/en/seasons/2018/REG/standings.json?api_key=" + os.environ.get('SPORTRADAR_KEY'))
 
-json_string = json.load(standings_file)
+    res = conn.getresponse()
+    data = res.read()
+    return data
 
-west_conference_standings = {}
-east_conference_standings = {}
+def parse_json(json_string):
+    west_conference_standings = {}
+    east_conference_standings = {}
 
-for conference in json_string["conferences"]:
-    for division in conference["divisions"]:
-        for team in division["teams"]:
-            team_name = team["name"]
-            team_conference_standing = team["calc_rank"]["conf_rank"]
-            if conference["name"].lower() == "western conference": 
-                west_conference_standings[team_conference_standing] = team_name
-            if conference["name"].lower() == "eastern conference":
-                east_conference_standings[team_conference_standing] = team_name
+    for conference in json_string["conferences"]:
+        for division in conference["divisions"]:
+            for team in division["teams"]:
+                team_name = team["name"]
+                team_conference_standing = team["calc_rank"]["conf_rank"]
+                if conference["name"].lower() == "western conference": 
+                    west_conference_standings[team_conference_standing] = team_name
+                if conference["name"].lower() == "eastern conference":
+                    east_conference_standings[team_conference_standing] = team_name
+    return west_conference_standings, east_conference_standings
 
-print("West Standings")
-for standing in range(1, 15+1):
-    team_name = west_conference_standings[standing]
-    print(team_name + ": " + str(standing))
+def main():
+    nba_data = get_standings()
+    west_conference_standings, east_conference_standings = parse_json(json.loads(nba_data))
 
-print()
-print("East Standings")
-for standing in range(1, 15+1):
-    team_name = east_conference_standings[standing]
-    print(team_name + ": " + str(standing))
+    print("West Standings")
+    for standing in range(1, 15+1):
+        team_name = west_conference_standings[standing]
+        print(team_name + ": " + str(standing))
 
+    print()
+    print("East Standings")
+    for standing in range(1, 15+1):
+        team_name = east_conference_standings[standing]
+        print(team_name + ": " + str(standing))
+
+main()
